6d0341c761a431e9be9edac86ec6b4c0857cd0b1
helping083 <helping083@gmail.com>
2020-08-16 09:20:21 +0300

    hotel overview

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 const sideNavLinks = document.querySelectorAll('.side-nav__item');
+const overviewIcons = document.querySelectorAll('.overview__icon-star');
+const overviewStarsContainer = document.querySelector('.overview__stars');
+let rating = 0;
 
 sideNavLinks.forEach((item) => {
   item.addEventListener('click',(e) => {
@@ -9,3 +12,28 @@ sideNavLinks.forEach((item) => {
     item.classList.add("side-nav__item--active");
   })
 });
+
+overviewStarsContainer.addEventListener('click', (e) => {
+  if (e.target.className.baseVal === 'overview__icon-star') {
+    setStars(e.target.dataset.star)
+  } else if (e.target.className.baseVal === "star") {
+    getParrentDataAttributes(e.target);
+  }
+});
+
+function getParrentDataAttributes(children) {
+  let closest = children.closest('svg')
+  setStars(closest.dataset.star);
+}
+
+function setStars(rating) {
+  let intRating = parseInt(rating, 10);
+  rating = intRating;
+  overviewIcons.forEach((item, index) => {
+    item.style.fill = "gray";
+    if (index + 1 > intRating) {
+      return;
+    }
+    item.style.fill = "red";
+  });
+}
